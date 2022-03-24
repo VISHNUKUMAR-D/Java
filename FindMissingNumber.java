b63c87e5922c86bab0e712919673a85a834edac0
VISHNUKUMAR D <84860384+VISHNUKUMAR-D@users.noreply.github.com>
2022-03-24 22:17:23 +0530

    Update FindMissingNumber.java

--- FindMissingNumber.java
+++ FindMissingNumber.java
@@ -1,18 +1,16 @@
-import java.util.*;
+import java.util.Scanner;
 public class FindMissingNumber{
     public static void main(String args[]){
     Scanner scan = new Scanner(System.in);
     System.out.print("ENTER INTEGER N : ");
     int n = scan.nextInt();
+    int TotalSum = 0,TempSum=0;
     System.out.println("ENTER INTEGER N-1 NUMBER THAT IS INCLUSIVE BETWEEN 1 TO N : ");
-    ArrayList<Integer> list =  new ArrayList<Integer>();
-    for(int i=1; i <= n; i++){
-        list.add(i);
-    }
-    System.out.println(list);
     for(int i=1; i<n; i++){
-        list.remove(new Integer(scan.nextInt()));
+        TotalSum+=i;
+        TempSum += scan.nextInt();
     }
-    System.out.println(list);
+    TotalSum+=n;
+    System.out.println("THE MISSING NUMBER IS : "+(TotalSum-TempSum));
 }
 }
